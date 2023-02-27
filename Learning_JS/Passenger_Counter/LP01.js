2770dc0fe3a5f327809cdb3718443b8970c7ffc2
sushilpandeyy <116426753+sushilpandeyy@users.noreply.github.com>
2023-02-27 13:33:37 +0530

    Create LP01.js

---
+++ Learning_JS/Passenger_Counter/LP01.js
@@ -0,0 +1,15 @@
+//document.getElementById("Counter").innerText=5
+let count = 0
+let s1=0
+let s2=0
+let s3=0
+function incre() {
+count=count+1
+document.getElementById("Counter").innerText=count
+}
+function S() {
+s3=s2
+s2=s1
+s1=count
+para.innerText=s1 + " - " + s2 + " - " +s3
+}
