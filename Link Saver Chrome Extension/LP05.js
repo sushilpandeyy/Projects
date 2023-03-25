fe2b26bee35f7a21db93447f5f87f5e62815673b
Sushil Pandey <contact.sushilpandey@gmail.com>
2023-03-25 10:28:31 +0530

    Trying to Add Delete Drop Box🔗

--- Link Saver Chrome Extension/LP05.js
+++ Link Saver Chrome Extension/LP05.js
@@ -1,6 +1,8 @@
 url =[]
+activetab="links"                                           //tag for storage
 let cin=url.length;
 let n=0
+
 if(localStorage.getItem('links')){                          //IF URL LIST IS ALREADY SAVED
         localStorage.getItem('links')
         const storedBlogs = JSON.parse(localStorage.getItem('links'))
@@ -16,6 +18,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs){     //GET
 
 const inputBtn = document.getElementById("input-btn")                      //CREATING BUTTON VALUE FOR INPUT
 const delbtn = document.getElementById("del-btn")                          //CREATING BUTTON VALYE FOR DELETE
+const dellinkbtn = document.getElementById("del-btn-fin")                   //CREATING BUTTON FOR SPECIFIC LINKS
 
 
 inputBtn.addEventListener("click", function sav(){                         //FUNCTION SAVE AND SHOWING ERROR
@@ -28,10 +31,12 @@ inputBtn.addEventListener("click", function sav(){                         //FUN
     }
    
 })
-delbtn.addEventListener("click", function del(){                               //DELETING VALUE
+function del(){                               //DELETING VALUE
     localStorage.clear('links')
     list.innerHTML = ""
-} )
+    window.close();
+}
+
 function save(){                                                            //SAVING AND SHOWING 
 url[cin]=document.getElementById("link").value;
 link.value=""
@@ -44,14 +49,50 @@ function Show(){                                                           //SHO
     let listItems = ""
         for (let i = 0; i < url.length; i++) 
         {
+            if(url[i]=="" || url[i]=="undefined" || url[i]=="NULL"){
+                continue
+            }
+            else{
        listItems += `
         <li class="list-group-item">
             <a target='_blank' href='${url[i]}'>
                 ${url[i]}
             </a>
         </li>
-    `
+    `   
+        }
         }
         list.innerHTML = listItems
-        
+        addlist()
     }
+
+function addlist(){
+    let lis=""
+    for(let i=0; i<url.length; i++){
+        let newOption = new Option(url[i],url[i]);    //FIRST IS TEXT AND SECOND IS VALUE
+        const select = document.querySelector('#select1'); 
+        select.add(newOption,undefined);
+    }
+     }
+    dellinkbtn.addEventListener("click", function droplink(){              //WHEN OPTION IS CLICKED AND ELEMENT IS REQUESTED TO  REMOVE
+        let n=0;
+        selectElement = document.querySelector('#select1')
+        output = selectElement.value
+        console.log(output)
+        if(output=='ALL'){
+            del()
+        }
+        else{
+            const index = url.indexOf(output);
+            if (index > -1) {
+              url.splice(index, 1);
+            }
+            console.log(url);
+            refresh()
+    }
+     })
+function refresh(){                                   //ADD ELEMENT IN DROP DOWN LIST
+    document.getElementById("select1").innerHTML = "<option value='ALL' selected>Delete All</option>";
+    save()
+    addlist()
+}
